cf3b6c70684eff12e327e55302ec2d6077fe4ef0
Akriti Sood <akritisood@Akritis-Air.lan>
2024-10-07 19:07:51 +0530

    MovieApplication - Adding the Description and installing react hook form

---
+++ src/Components/Description.tsx
@@ -0,0 +1,17 @@
+const Description = () => {
+  return (
+    <div className="h-96 w-[38rem] border-2 border-pink-200">
+      <span className="text-5xl font-extrabold block p-2 text-center">
+        Unlimited movies, TV shows and more
+      </span>
+      <span className="text-lg font-semibold block p-2 text-center">
+        Starts at ₹149. Cancel at any time.
+      </span>
+      <span className="block p-2 text-center">
+        Ready to watch? Enter your email to create or restart your membership.
+      </span>
+    </div>
+  );
+};
+
+export default Description;
--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,9 +1,20 @@
+import Description from "./Description";
 import Header from "./Header";
 
 const LandingPage = () => {
   return (
-    <div>
-      <Header shouldShowSignInButton={true} />
+    <div className="flex flex-col h-screen">
+      <div className="border-2 border-black h-20">
+        <Header shouldShowSignInButton={true} />
+      </div>
+      <div
+        className="border-2 border-black flex items-center justify-center"
+        style={{
+          height: `calc(100vh - 5rem)`,
+        }}
+      >
+        <Description />
+      </div>
     </div>
   );
 };
