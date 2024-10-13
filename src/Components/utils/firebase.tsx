92ec0030a9420c17bd613dd79ed0dfe4c533efba
Akriti Sood <akritisood@Akritis-Air.lan>
2024-10-13 15:31:47 +0530

    MovieApplication - removing firebase config

--- src/Components/utils/firebase.tsx
+++ src/Components/utils/firebase.tsx
@@ -5,14 +5,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCqr4hojrNqCrYq8d5i6b9wo4CP0EzFipo",
-  authDomain: "movienight-475ab.firebaseapp.com",
-  projectId: "movienight-475ab",
-  storageBucket: "movienight-475ab.appspot.com",
-  messagingSenderId: "523147710209",
-  appId: "1:523147710209:web:79aa5dc4772e12e7301500",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
