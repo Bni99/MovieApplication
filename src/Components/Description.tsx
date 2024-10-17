d5684c8c541f39c4be873bdcd11ed17ae88aeb9d
Akriti Sood <akritisood@Akritis-Air.lan>
2024-10-17 19:09:10 +0530

    MovieApplication - fixing a failing test

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -1,4 +1,9 @@
 import DescriptionForm from "./DescriptionForm";
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 const Description = () => {
   return (
