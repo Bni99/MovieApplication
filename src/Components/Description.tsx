379583a2bfcd8698203e479b67e6554daeeec616
Akriti Sood <akritisood@Akritis-Air.lan>
2024-10-17 19:22:57 +0530

    MovieApplication - removing jest from component

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -1,9 +1,4 @@
 import DescriptionForm from "./DescriptionForm";
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockNavigate,
-}));
 
 const Description = () => {
   return (
